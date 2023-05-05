de32a0b870221328f3a8b0f22aacba5619677b95
Sparklez_Foxy <fionahii@outlook.my>
2023-05-05 17:45:11 +0800

    Main update+register bg

--- CAMS.py
+++ CAMS.py
@@ -1,34 +1,111 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_manager
+import sqlite3 as sql
+import re
+
 app = Flask(__name__,
             static_url_path='', 
             static_folder='web/static',
             template_folder='web/templates')
 
+app.secret_key = 'my-secret-key'
+app.config['DATABASES'] = {
+   'users': 'databases/users.db',
+   'history': 'databases/history.db',
+   'entries': 'databases/entries.db'
+}
+app.config['CURRENT_DATABASE'] = None
+
+def get_db(database_name):
+   """Connect to a specific SQLite database."""
+   if database_name not in app.config['DATABASES']:
+         raise ValueError(f"Database '{database_name}' not configured")
+   if app.config['CURRENT_DATABASE'] != database_name:
+         close_db()
+         db = g._database = sql.connect(app.config['DATABASES'][database_name])
+         g._database_name = database_name
+         app.config['CURRENT_DATABASE'] = database_name
+   else:
+         db = getattr(g, '_database', None)
+         if db is None:
+            db = g._database = sql.connect(app.config['DATABASES'][database_name])
+            g._database_name = database_name
+            app.config['CURRENT_DATABASE'] = database_name
+   return db
+
+def close_db():
+   """Close the database connection."""
+   db = getattr(g, '_database', None)
+   if db is not None:
+      db.close()
+      app.config['CURRENT_DATABASE'] = None
+
 @app.route('/')
-def start():
-
-
+def home():
    r = app.make_response(render_template('index.html'))
 
    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
    r.headers["Pragma"] = "no-cache"
    r.headers["Expires"] = "0"
-
+   
    return r
 
-'''
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-   if request.method == 'POST':
-      session['username'] = request.form['username']
-      return redirect(url_for('index'))
-   return
-'''
+   if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+         username = request.form['username']
+         password = request.form['password']
+         db = get_db("users")
+         cursor = db.cursor()
+         cursor.execute('SELECT * FROM accounts WHERE username = ?', (username,))
+         while (account := cursor.fetchone()):
+            if check_password_hash(account[2], password):
+               session['logged_in'] = True
+               session['username'] = account[1]
+               session['colour_1'] = account[3]
+               session['colour_2'] = account[4]
+               return "Login successful"
+         error_message = "Incorrect username / password"
+         return error_message, 400
+   return redirect(url_for("home"))
+
+ 
+@app.route('/logout')
+def logout():
+   session.pop('logged_in', None)
+   session.pop('username', None)
+   session.pop('colour_1', None)
+   session.pop('colour_2', None)
+   return redirect(url_for('login'))
+ 
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+   if request.method == 'POST' and 'username' in request.form and 'password' in request.form :
+      username = request.form['username']
+      password = request.form['password']
+      db = get_db("users")
+      cursor = db.cursor()
+      cursor.execute('SELECT * FROM accounts WHERE username = ?', (username,))
+      account = cursor.fetchone()
+      if account:
+         error_message = 'Account already exists !'
+         return error_message, 400
+      elif not re.match(r'[A-Za-z0-9]+', username): #can be done in js
+         msg = 'Username must contain only characters and numbers !'
+      else:
+         cursor.execute('INSERT INTO accounts (username,password) VALUES (?,?)', (username, generate_password_hash(password),))
+         db.commit()
+         session['logged_in'] = True
+         session['username'] = account[1]
+         return "Login successful"
+   return render_template('register.html')
+
 
 @app.route('/quiz')
 def quiz():
 
-   return render_template('test.html')
+   return render_template('quiz.html')
 
 
 
@@ -48,5 +125,3 @@ def history():
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
--- web/static/js/default.js
+++ web/static/js/default.js
@@ -1,5 +1,9 @@
-var sideNav = document.querySelector('.side_nav');
+var side_nav = document.querySelector('.side_nav');
 var nav_width = $(".side_nav").width();
+
+var login_form = document.getElementById('login_form');
+var sn = document.getElementById('sn');
+
 
 function window_ratio(){
   if(window.innerHeight / window.innerWidth > 2160 /3840){
@@ -33,7 +37,7 @@ function resize_bg(){
   
   if(window_skinny()){
     $(".side_nav a").css({"text-align": "center", "font-size": "40px"});
-    if (sideNav.style.width === nav_width) {
+    if (side_nav.style.width === nav_width) {
       nav_width = "100vw";
       $(".side_nav").width(nav_width);
       $("#start_button").fadeOut(50);
@@ -43,7 +47,7 @@ function resize_bg(){
 
   else{
     $(".side_nav a").css({"text-align": "left", "font-size": "30px"});
-    if (sideNav.style.width === nav_width) {
+    if (side_nav.style.width === nav_width) {
       nav_width = "250px"
       $(".side_nav").width(nav_width);
       $("#start_button").fadeIn(50);
@@ -51,6 +55,28 @@ function resize_bg(){
     nav_width = "250px"
   }
 }
+
+function active_sn(){
+  $('.hamburger').toggleClass('active');
+  if (side_nav.style.width === nav_width) {
+    $(".side_nav").width("0");
+    $("#sb").fadeIn(50);
+    $("#hb").fadeIn(50);
+    $("#start_button").fadeIn(50);
+    $('#sn').css({"display":"none"});
+    $('.modal').css({"background-color":"rgba(65, 42, 19, 0.4)"});
+  }
+  else {
+    $(".side_nav").width(nav_width);
+    $("#sb").fadeOut(50);
+    $("#hb").fadeOut(50);
+    $('#sn').css({"display":"block"});
+    $('.modal').css({"background-color":"rgba(65, 42, 19, 0.0)"});
+    if(window_skinny()){
+      $("#start_button").fadeOut(50);
+    }
+  }
+};
 
 $(document).ready(function() {
   resize_bg();
@@ -61,27 +87,85 @@ $( window ).on( "resize", function(){
 });
 
 $('.hamburger').click(function() {
-  if (sideNav.style.width === nav_width) {
-    $(".side_nav").width("0");
-    $("#sb").fadeIn(50);
-    $("#hb").fadeIn(50);
-    $("#start_button").fadeIn(50);
-  } 
-  else {
-    $(".side_nav").width(nav_width);
-    $("#sb").fadeOut(50);
-    $("#hb").fadeOut(50);
-    if(window_skinny()){
-      $("#start_button").fadeOut(50);
+  active_sn();
+});
+
+$('#sb').click(function() {
+  $('#login_form').css({"display":"block"});
+  $('.modal').css({"z-index":"12"});
+});
+
+$('.home_button').click(function() {
+  window.location.href = "/";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+
+$(document).click(function(event) {
+  if (event.target == login_form) {
+    $('#login_form').css({"display":"none"});
+    $('.modal').css({"z-index":"9"});
+  }
+  else if (event.target == sn){
+    if (side_nav.style.width === nav_width) {
+      active_sn();
     }
   }
 });
 
-$('.login_button').click(function() {
-window.location.href = "/login";
+
+$('.x_container').click(function() {
+  $('#login_form').css({"display":"none"});
+  $('.modal').css({"z-index":"9"});
 });
 
-$('.home_button').click(function() {
-window.location.href = "/";
+$("#login_form").submit(async function(event) {
+  event.preventDefault();
+
+  // get the form data
+  const formData = new FormData(event.target);
+
+  // send a POST request to the server
+  const response = await fetch('/login', {
+    method: 'POST',
+    body: formData
+  });
+
+  // handle the server response
+  if (response.ok) {
+    // success, redirect to the home page
+    window.location.href = "/";
+  } 
+  else {
+    // error, update the form with the error message
+    const errorMessage = await response.text();
+    const errorElement = document.createElement("p");
+    errorElement.textContent = errorMessage;
+    event.target.prepend(errorElement);
+  }
 });
 
+$("#register_form").submit(async function(event) {
+  event.preventDefault();
+
+  // get the form data
+  const formData = new FormData(event.target);
+
+  // send a POST request to the server
+  const response = await fetch('/register', {
+    method: 'POST',
+    body: formData
+  });
+
+  // handle the server response
+  if (response.ok) {
+    // success, redirect to the home page
+    window.location.href = "/";
+  } else {
+    // error, update the form with the error message
+    const errorMessage = await response.text();
+    const errorElement = document.createElement("p");
+    errorElement.textContent = errorMessage;
+    event.target.prepend(errorElement);
+  }
+});
